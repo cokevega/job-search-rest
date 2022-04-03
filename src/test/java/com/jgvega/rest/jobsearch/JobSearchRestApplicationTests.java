5bbed48f4b07cdb8f567f8d23539117e749c9ece
Jorge Garcia Vega <jorgegavega@hotmail.com>
2022-04-03 22:16:49 +0200

    Initial commit

---
+++ src/test/java/com/jgvega/rest/jobsearch/JobSearchRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgvega.rest.jobsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
